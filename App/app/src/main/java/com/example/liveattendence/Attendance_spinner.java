acf962f6661f7817f82e3051b247b321369f671d
jasim-fayas <jazzz7080@gmail.com>
2024-03-20 17:03:57 +0530

    20thUpadteAfterAttendance

--- App/app/src/main/java/com/example/liveattendence/Attendance_spinner.java
+++ App/app/src/main/java/com/example/liveattendence/Attendance_spinner.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class faculty_daily_course extends AppCompatActivity {
+public class Attendance_spinner extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_faculty_daily_course);
+        setContentView(R.layout.activity_attendance_spinner);
     }
 }
